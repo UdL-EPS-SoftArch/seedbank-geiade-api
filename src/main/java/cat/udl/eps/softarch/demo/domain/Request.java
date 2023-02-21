6c10f26eba33beae9bc09e5f3229cfbbb6537f26
Pablo <->
2023-02-21 18:43:59 +0100

    Implement Request class

---
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Request.java
@@ -0,0 +1,22 @@
+package cat.udl.eps.softarch.demo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "Request")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Request extends Batch {
+    @Id
+    private Long id;
+
+    private ZonedDateTime lastUpdate;
+}
