c0e3dc678a09f3a3d7c1c8e159f0bd3bc39bca5e
jordicolome <jcolomemontero@gmail.com>
2023-02-21 18:23:58 +0100

    Add status to DonationEntity

--- src/main/java/cat/udl/eps/softarch/demo/domain/DonationEntity.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/DonationEntity.java
@@ -1,17 +1,19 @@
 package cat.udl.eps.softarch.demo.domain;
 
+import cat.udl.eps.softarch.demo.utils.DonationStatus;
 import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-//import javax.persistence.Table;
+import javax.persistence.Table;
 
 @Entity
-//@Table(name = "Donation")
+@Table(name = "Donation")
 @Data
 public class DonationEntity /*extends Batch*/{
     //private Donor donor;
     //private Batch batch;
     @Id
     private long id;
+    private DonationStatus donationStatus = DonationStatus.PENDING;
 }
