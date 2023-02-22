0e8a598042f67d8cffb38afc444f9d9e0db498c5
sala1714 <pepesalamarques@gmail.com>
2023-02-22 18:37:33 +0100

    Adding decorators and imports.

--- src/main/java/cat/udl/eps/softarch/demo/domain/Seed.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Seed.java
@@ -1,9 +1,28 @@
 package cat.udl.eps.softarch.demo.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import java.util.List;
 
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+@AllArgsConstructor
 public class Seed {
+    @Id
+    private Long id;
     private String scientificName;
-    private String[] commonNames;
+    @ElementCollection
+    private List<String> commonNames;
+    @ManyToMany
     private List<Seed> beneficialFor;
+
 }
