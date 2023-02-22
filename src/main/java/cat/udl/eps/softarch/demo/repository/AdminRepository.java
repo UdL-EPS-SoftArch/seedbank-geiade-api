4d6cf7154a860fdb31f686beecade536d742d727
Ilyass Mahdiyan <ilyassmah00@gmail.com>
2023-02-22 18:16:31 +0100

    Create AdminRepository

---
+++ src/main/java/cat/udl/eps/softarch/demo/repository/AdminRepository.java
@@ -0,0 +1,7 @@
+package cat.udl.eps.softarch.demo.repository;
+
+import cat.udl.eps.softarch.demo.domain.Admin;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface AdminRepository extends PagingAndSortingRepository<Admin, Integer> {
+}
