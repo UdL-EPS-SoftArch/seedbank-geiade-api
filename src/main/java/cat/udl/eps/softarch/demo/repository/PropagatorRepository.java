251c35053a8d29a33697bf1d83889b0f614d8f1a
Sergi <sergiedits@gmail.com>
2023-02-23 16:38:50 +0100

    Created Interface for PropagatorRepository

--- src/main/java/cat/udl/eps/softarch/demo/repository/PropagatorRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/PropagatorRepository.java
@@ -4,8 +4,6 @@ import cat.udl.eps.softarch.demo.domain.Propagator;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-
-
 @RepositoryRestResource
 public interface PropagatorRepository extends PagingAndSortingRepository<Propagator, String> {
 
