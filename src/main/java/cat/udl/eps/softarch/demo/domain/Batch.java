0069b9cf9a7eb21fea4708b527338e073b3393f2
llobera00 <o.llobera00@gmail.com>
2023-02-23 16:22:03 +0100

    Created relation between Seed Class and Batch class

--- src/main/java/cat/udl/eps/softarch/demo/domain/Batch.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Batch.java
@@ -14,6 +14,7 @@ import java.time.ZonedDateTime;
 public class Batch {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+
     private long id;
     private int amount = -1;
     private long latitude;
@@ -21,7 +22,7 @@ public class Batch {
     private ZonedDateTime date;
 
     /*
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @ManyToOne(cascade = CascadeType.ALL, orphanRemoval = true)
     @JoinColumn(name = "batch_id")
     private List<Seed> seeds;
     */
