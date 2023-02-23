cdb6e589dc7b6477c9fcea262ab01c1e112eaa87
jaumeguasch <jguaschllobera@gmail.com>
2023-02-23 16:04:21 +0100

    Created some tests for batch class

--- src/main/java/cat/udl/eps/softarch/demo/domain/Batch.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Batch.java
@@ -1,14 +1,21 @@
 package cat.udl.eps.softarch.demo.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.time.ZonedDateTime;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Batch {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-    private int amount;
+    private int amount = -1;
     private long latitude;
     private long longitude;
     private ZonedDateTime date;
---
+++ src/test/java/cat/udl/eps/softarch/demo/steps/RegisterBatchStepDefs.java
@@ -0,0 +1,20 @@
+package cat.udl.eps.softarch.demo.steps;
+
+import cat.udl.eps.softarch.demo.domain.Batch;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class RegisterBatchStepDefs {
+    public static Batch batch;
+
+    @When("^I register a new batch$")
+    public void registration(){
+        batch = new Batch();
+    }
+    @Then("^Amount is -1$")
+    public void equalsAmount(){
+        assertThat(batch.getAmount(), is(-1));
+    }
+}
