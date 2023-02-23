2949f16c7701c30340b10148613d0eb815e425c2
Ilyass Mahdiyan <ilyassmah00@gmail.com>
2023-02-23 16:39:22 +0100

    Annotations to generate the REST API for respositories

--- src/main/java/cat/udl/eps/softarch/demo/repository/AdminRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/AdminRepository.java
@@ -2,6 +2,8 @@ package cat.udl.eps.softarch.demo.repository;
 
 import cat.udl.eps.softarch.demo.domain.Admin;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface AdminRepository extends PagingAndSortingRepository<Admin, String> {
 }
