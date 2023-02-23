a4bd8d1e48cb8a33c0776ea451028364f697fb73
Sergi <sergiedits@gmail.com>
2023-02-23 16:27:23 +0100

    Created Interface for PropagatorRepository

--- src/main/java/cat/udl/eps/softarch/demo/repository/PropagatorRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/PropagatorRepository.java
@@ -1,23 +1,15 @@
 package cat.udl.eps.softarch.demo.repository;
 
 import cat.udl.eps.softarch.demo.domain.Propagator;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-/*
+
 
 @RepositoryRestResource
-public interface PropagatorRepository extends PagingAndSortingRepository<Propagator, Long> {
-         Interface provides automatically, as defined in https://docs.spring.io/spring-data/commons/docs/current/api/org/springframework/data/repository/PagingAndSortingRepository.html
-         * count, delete, deleteAll, deleteById, existsById, findAll, findAllById, findById, save, saveAll
-         *
-         * Additional methods following the syntax defined in
-         * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation
+public interface PropagatorRepository extends PagingAndSortingRepository<Propagator, String> {
 
-
-        List<Propagator> findByUsernameContaining(@Param("text") String text);
-        }
 }
 
-* */
+
 
