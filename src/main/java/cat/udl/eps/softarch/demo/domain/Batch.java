f9ce7c782b4701965c5501d6e739cd4a5d02a432
llobera00 <o.llobera00@gmail.com>
2023-03-02 16:01:37 +0100

    Changed Batch parameters, adapted with the UML

--- src/main/java/cat/udl/eps/softarch/demo/domain/Batch.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Batch.java
@@ -3,8 +3,12 @@ package cat.udl.eps.softarch.demo.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.ZonedDateTime;
 
 @Entity
@@ -16,10 +20,19 @@ public class Batch {
     @GeneratedValue(strategy = GenerationType.AUTO)
 
     private Long id;
-    private int amount = -1;
-    private Long latitude;
-    private Long longitude;
-    private ZonedDateTime date;
+    @NotNull
+    @Min(value = 1, message = "There must be one or more batches")
+    private Integer amount;
+
+    @NotNull
+    @Min(value = 0, message = "Weight must be positive")
+    private Float weight;
+
+    @NotBlank
+    @Length(min = 2, max = 30)
+    private String location;
+
+    private ZonedDateTime date = ZonedDateTime.now();
 
     /*
     private List<Seed> seeds;
