f7a9e4a29bb2f4b6df7306bd01a43fe0c777665f
Pablo <->
2023-03-07 18:43:29 +0100

    Implement propagator feature

---
+++ src/test/java/cat/udl/eps/softarch/demo/steps/PropagatorStepDefs.java
@@ -0,0 +1,84 @@
+package cat.udl.eps.softarch.demo.steps;
+
+import cat.udl.eps.softarch.demo.domain.Propagator;
+import cat.udl.eps.softarch.demo.repository.PropagatorRepository;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class PropagatorStepDefs {
+
+  @Autowired
+  private StepDefs stepDefs;
+
+  @Autowired
+  private PropagatorRepository propagatorRepository;
+
+  @Given("^There is no registered propagator with username \"([^\"]*)\"$")
+  public void thereIsNoRegisteredPropagatorWithUsername(String username) {
+    Assert.assertFalse("Propagator \""
+                    +  username + "\"shouldn't exist",
+                    propagatorRepository.existsById(username));
+  }
+
+  @Given("^There is a registered propagator with username \"([^\"]*)\" and password \"([^\"]*)\" and email \"([^\"]*)\"$")
+  public void thereIsARegisteredPropagatorWithUsernameAndPasswordAndEmail(String username, String password, String email) {
+    if (!propagatorRepository.existsById(username)) {
+      Propagator propagator = new Propagator();
+      propagator.setEmail(email);
+      propagator.setUsername(username);
+      propagator.setPassword(password);
+      propagator.encodePassword();
+      propagatorRepository.save(propagator);
+    }
+  }
+
+  @When("^I register a new propagator with username \"([^\"]*)\", email \"([^\"]*)\" and password \"([^\"]*)\"$")
+  public void iRegisterANewPropagatorWithUsernameEmailAndPassword(String username, String email, String password) throws Throwable {
+    Propagator propagator = new Propagator();
+    propagator.setUsername(username);
+    propagator.setEmail(email);
+    propagatorRepository.save(propagator);
+
+    stepDefs.result = stepDefs.mockMvc.perform(
+            post("/propagators")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(new JSONObject(
+                            stepDefs.mapper.writeValueAsString(propagator)
+                    ).put("password", password).toString())
+                    .accept(MediaType.APPLICATION_JSON)
+                    .with(AuthenticationStepDefs.authenticate()))
+            .andDo(print());
+  }
+
+  @And("^It has been created a propagator with username \"([^\"]*)\" and email \"([^\"]*)\", the password is not returned$")
+  public void itHasBeenCreatedAPropagatorWithUsername(String username, String email) throws Throwable {
+    stepDefs.result = stepDefs.mockMvc.perform(
+            get("/propagators/{username}", username)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .with(AuthenticationStepDefs.authenticate()))
+            .andDo(print())
+            .andExpect(jsonPath("$.email", is(email)))
+            .andExpect(jsonPath("$.password").doesNotExist());
+  }
+
+  @And("^It has not been created a propagator with username \"([^\"]*)\"$")
+  public void itHasNotBeenCreatedAPropagatorWithUsername(String username) throws Throwable {
+    stepDefs.result = stepDefs.mockMvc.perform(
+            get("/propagators/{username}", username)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .with(AuthenticationStepDefs.authenticate()))
+            .andExpect(status().isNotFound());
+  }
+}
---
+++ src/test/resources/features/Propagator.feature
@@ -0,0 +1,25 @@
+Feature: Register Propagator
+  In order to use the app
+  As a propagator
+  I want to register myself and get a propagator account
+
+  Scenario: Register new propagator
+    Given There is no registered propagator with username "propagator1"
+    And I'm not logged in
+    When I register a new propagator with username "propagator1", email "propagator1@sample.app" and password "password"
+    Then The response code is 201
+    And It has been created a propagator with username "propagator1" and email "propagator1@sample.app", the password is not returned
+    And I can login with username "propagator1" and password "password"
+
+  Scenario: Register existing propagator
+    Given There is a registered propagator with username "propagator2" and password "existing" and email "propagator2@sample.app"
+    And I'm not logged in
+    When I register a new propagator with username "propagator2", email "propagator2@sample.app" and password "newpassword"
+    Then The response code is 409
+    And I cannot login with username "propagator2" and password "newpassword"
+
+  Scenario: Register propagator when already authenticated
+    Given I login as "propagator" with password "password"
+    When I register a new propagator with username "propagator3", email "propagator3@sample.app" and password "password"
+    Then The response code is 403
+    And It has not been created a propagator with username "user"
