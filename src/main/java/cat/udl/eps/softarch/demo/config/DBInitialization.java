67229fabdd0ecfc9d230edb9c2447c5b39e50e42
GerardCasta <gerardca93@gmail.com>
2023-03-08 18:43:43 +0100

    Create Donor in database

--- src/main/java/cat/udl/eps/softarch/demo/config/DBInitialization.java
+++ src/main/java/cat/udl/eps/softarch/demo/config/DBInitialization.java
@@ -1,5 +1,7 @@
 package cat.udl.eps.softarch.demo.config;
+import cat.udl.eps.softarch.demo.domain.Donor;
 import cat.udl.eps.softarch.demo.domain.User;
+import cat.udl.eps.softarch.demo.repository.DonorRepository;
 import cat.udl.eps.softarch.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -13,9 +15,11 @@ public class DBInitialization {
     @Value("${spring.profiles.active:}")
     private String activeProfiles;
     private final UserRepository userRepository;
+    private final DonorRepository donorRepository;
 
-    public DBInitialization(UserRepository userRepository) {
+    public DBInitialization(UserRepository userRepository, DonorRepository donorRepository) {
         this.userRepository = userRepository;
+        this.donorRepository = donorRepository;
     }
 
     @PostConstruct
@@ -29,6 +33,17 @@ public class DBInitialization {
             user.encodePassword();
             userRepository.save(user);
         }
+
+        // Default donor
+        if (!donorRepository.existsById("userdonor")) {
+            Donor donor = new Donor();
+            donor.setEmail("userdonor@sample.app");
+            donor.setUsername("userdonor");
+            donor.setPassword(defaultPassword);
+            donor.encodePassword();
+            donorRepository.save(donor);
+        }
+
         if (Arrays.asList(activeProfiles.split(",")).contains("test")) {
             // Testing instances
             if (!userRepository.existsById("test")) {
