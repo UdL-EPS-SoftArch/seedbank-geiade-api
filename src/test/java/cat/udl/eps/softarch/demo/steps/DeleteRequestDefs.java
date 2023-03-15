96187b8ffad8dc870ded382d89833290ec90ae3b
sala1714 <pepesalamarques@gmail.com>
2023-03-15 18:20:41 +0100

    DeleteRequest Step Defs test definition

---
+++ src/test/java/cat/udl/eps/softarch/demo/steps/DeleteRequestDefs.java
@@ -0,0 +1,35 @@
+package cat.udl.eps.softarch.demo.steps;
+
+import cat.udl.eps.softarch.demo.domain.Request;
+import cat.udl.eps.softarch.demo.repository.PropagatorRepository;
+import cat.udl.eps.softarch.demo.repository.RequestRepository;
+import cat.udl.eps.softarch.demo.repository.TakeRepository;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+public class DeleteRequestDefs {
+    @Autowired
+    private StepDefs stepDefs;
+    @Autowired
+    private RequestRepository requestRepository;
+    @Given("^There is a Request created with Id \"([^\"]*)\"$")
+    public void thereIsARequestCreatedWithId(Long id) throws RuntimeException {
+        if (!requestRepository.existsById(id)) {
+            throw new RuntimeException("Unexisting Request");
+        }
+    }
+    @When("^I delete request with id \"([^\"]*)\"$")
+    public void deleteRequest(String id) throws Exception {
+        stepDefs.result = stepDefs.mockMvc.perform(
+                        delete("/requests", id)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print());
+    }
+}
