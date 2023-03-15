fdf98713511ac16f0ec8bffa6f6faa3cf7dd8d9c
sala1714 <pepesalamarques@gmail.com>
2023-03-15 17:48:41 +0100

    RequestHandler before creation definition

--- src/main/java/cat/udl/eps/softarch/demo/handler/RequestEventHandler.java
+++ src/main/java/cat/udl/eps/softarch/demo/handler/RequestEventHandler.java
@@ -1,14 +1,11 @@
 package cat.udl.eps.softarch.demo.handler;
 
 import cat.udl.eps.softarch.demo.domain.Request;
-import cat.udl.eps.softarch.demo.domain.Take;
-import cat.udl.eps.softarch.demo.domain.User;
+import cat.udl.eps.softarch.demo.exception.ForbiddenException;
 import cat.udl.eps.softarch.demo.repository.RequestRepository;
-import cat.udl.eps.softarch.demo.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.rest.core.annotation.*;
-
 import java.util.Optional;
 
 public class RequestEventHandler {
@@ -24,9 +21,8 @@ public class RequestEventHandler {
     public void handleRequestPreCreate(Request request) {
         logger.info("Before creating: {}", request.toString());
         Optional<Request> request1 = requestRepository.findByFulfilledBy(request.getFulfilledBy());
-        if(){
-
+        if(request1.isPresent()){
+            throw new ForbiddenException();
         }
     }
-
 }
