2cd81517d8777373fa7d276b0f426b2a3b7b9020
sala1714 <pepesalamarques@gmail.com>
2023-03-15 17:30:14 +0100

    Adapt Request handler methods.

--- src/main/java/cat/udl/eps/softarch/demo/handler/RequestEventHandler.java
+++ src/main/java/cat/udl/eps/softarch/demo/handler/RequestEventHandler.java
@@ -1,4 +1,24 @@
 package cat.udl.eps.softarch.demo.handler;
 
+import cat.udl.eps.softarch.demo.domain.User;
+import cat.udl.eps.softarch.demo.repository.RequestRepository;
+import cat.udl.eps.softarch.demo.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.rest.core.annotation.*;
+
 public class RequestEventHandler {
+    final Logger logger = LoggerFactory.getLogger(User.class);
+
+    final RequestRepository requestRepository;
+
+    public RequestEventHandler(RequestRepository requestRepository) {
+        this.requestRepository = requestRepository;
+    }
+
+    @HandleBeforeCreate
+    public void handleRequestPreCreate(User user) {
+        logger.info("Before creating: {}", user.toString());
+    }
+
 }
