d9507c24968865038a9ecaa3bb612922a10f009c
sala1714 <pepesalamarques@gmail.com>
2023-03-16 16:33:45 +0100

    CreateTake comments deletion

--- src/test/java/cat/udl/eps/softarch/demo/steps/CreateTakeStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/demo/steps/CreateTakeStepDefs.java
@@ -2,9 +2,7 @@ package cat.udl.eps.softarch.demo.steps;
 
 import cat.udl.eps.softarch.demo.domain.Propagator;
 import cat.udl.eps.softarch.demo.domain.Take;
-import cat.udl.eps.softarch.demo.repository.PropagatorRepository;
 import cat.udl.eps.softarch.demo.repository.TakeRepository;
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
@@ -21,46 +19,33 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 
 public class CreateTakeStepDefs {
     @Autowired
-    private PropagatorRepository propagatorRepository;
-    @Autowired
     private TakeRepository takeRepository;
     @Autowired
     private StepDefs stepDefs;
 
-    /*
-    @Given("^There is a registered propagator with username \"([^\"]*)\"$")
-    public void thereIsARegisteredUserWithUsername(String username) throws RuntimeException {
-        if (!propagatorRepository.existsById(username)) {
-            throw new RuntimeException("Unregistered propagator");
-        }
-    }
-
-     */
-
     @When("^I create a new valid Take with Propagator$")
     public void createTake() throws Exception {
-            Take take = createValidTake("Lleida");
-            stepDefs.result = stepDefs.mockMvc.perform(
-                            post("/takes")
-                                    .contentType(MediaType.APPLICATION_JSON_VALUE)
-                                    .characterEncoding("utf-8")
-                                    .content(stepDefs.mapper.writeValueAsString(take))
-                                    .accept(MediaType.APPLICATION_JSON)
-                                    .with(AuthenticationStepDefs.authenticate()))
-                    .andDo(print());
+        Take take = createValidTake("Lleida");
+        stepDefs.result = stepDefs.mockMvc.perform(
+                        post("/takes")
+                                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                                .characterEncoding("utf-8")
+                                .content(stepDefs.mapper.writeValueAsString(take))
+                                .accept(MediaType.APPLICATION_JSON)
+                                .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print());
     }
 
 
     @Then("There is 1 take created$")
-    public void thereIsOnetakeCreated() throws Exception{
+    public void thereIsOnetakeCreated() throws Exception {
         Assert.assertEquals(1, takeRepository.count());
     }
 
-
     @When("I create 5 takes")
-    public void thereAreVariousTakesCreated() throws Exception{
+    public void thereAreVariousTakesCreated() throws Exception {
         List<String> locations = getLocations();
-        for (int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             Take take = createValidTake(locations.get(i));
             stepDefs.result = stepDefs.mockMvc.perform(
                             post("/takes")
@@ -74,11 +59,11 @@ public class CreateTakeStepDefs {
     }
 
     @Then("There are 5 take created$")
-    public void thereAreFiveTakeCreated() throws Exception{
+    public void thereAreFiveTakeCreated() throws Exception {
         Assert.assertEquals(5, takeRepository.count());
     }
 
-    private Take createValidTake(String location){
+    private Take createValidTake(String location) {
         Take take = new Take();
         take.setWeight(BigDecimal.TEN);
         take.setAmount(10);
@@ -87,6 +72,7 @@ public class CreateTakeStepDefs {
         take.setBy(createValidPropagator());
         return take;
     }
+
     private Propagator createValidPropagator() {
         Propagator propagator = new Propagator();
         propagator.setUsername("propagator");
