ce76cdad6c43aee891eb2f7ff4bd3e481adbf453
Sergi <sergiedits@gmail.com>
2023-03-23 16:02:28 +0100

    Deleted some Take scenarios

--- src/test/java/cat/udl/eps/softarch/demo/steps/CreateTakeStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/demo/steps/CreateTakeStepDefs.java
@@ -4,7 +4,6 @@ import cat.udl.eps.softarch.demo.domain.Propagator;
 import cat.udl.eps.softarch.demo.domain.Take;
 import cat.udl.eps.softarch.demo.repository.PropagatorRepository;
 import cat.udl.eps.softarch.demo.repository.TakeRepository;
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
@@ -29,7 +28,7 @@ public class CreateTakeStepDefs {
 
     @When("^I create a new valid Take with Propagator$")
     public void createTake() throws Exception {
-        Take take = createValidTake("Lleida");
+        Take take = createValidTakeWithPropagator("Lleida");
         stepDefs.result = stepDefs.mockMvc.perform(
                         post("/takes")
                                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -38,6 +37,7 @@ public class CreateTakeStepDefs {
                                 .accept(MediaType.APPLICATION_JSON)
                                 .with(AuthenticationStepDefs.authenticate()))
                 .andDo(print());
+        //newResourceUri = stepDefs.result.andReturn().getResponse().getHeader("Location");
     }
 
     @Then("There is 1 take created$")
@@ -49,7 +49,7 @@ public class CreateTakeStepDefs {
     public void thereAreVariousTakesCreated() throws Exception {
         List<String> locations = getLocations();
         for (int i = 0; i < 5; i++) {
-            Take take = createValidTake(locations.get(i));
+            Take take = createValidTakeWithPropagator(locations.get(i));
             stepDefs.result = stepDefs.mockMvc.perform(
                             post("/takes")
                                     .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -74,23 +74,11 @@ public class CreateTakeStepDefs {
                 .andDo(print());
     }
 
-    @When("I create a take without a Propagator")
-    public void CreateATakeWithoutAPropagator() throws Exception {
-        Take take = createValidTakeWithoutPropagator("Lleida");
-        stepDefs.result = stepDefs.mockMvc.perform(
-                        post("/takes")
-                                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                                .characterEncoding("utf-8")
-                                .content(stepDefs.mapper.writeValueAsString(take))
-                                .accept(MediaType.APPLICATION_JSON)
-                                .with(AuthenticationStepDefs.authenticate()))
-                .andDo(print());
-    }
 
-
+    /*
     @When("I create a new valid Take with Donor")
     public void CreateANewValidTakeWithDonor() throws Exception {
-        Take take = createValidTake("Lleida");
+        Take take = createValidTakeWithDonor("Lleida");
         stepDefs.result = stepDefs.mockMvc.perform(
                         post("/takes")
                                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -99,8 +87,8 @@ public class CreateTakeStepDefs {
                                 .accept(MediaType.APPLICATION_JSON)
                                 .with(AuthenticationStepDefs.authenticate()))
                 .andDo(print());
-
     }
+     */
 
     @Then("There are 5 take created$")
     public void thereAreFiveTakeCreated() throws Exception {
@@ -117,17 +105,17 @@ public class CreateTakeStepDefs {
         take.setWeight(BigDecimal.TEN);
         take.setLocation(location);
         take.setDate(ZonedDateTime.now());
-        take.setPropagator(createValidPropagator());
+        take.setBy(createValidPropagator());
         return take;
     }
 
-    private Take createValidTake(String location) {
+    private Take createValidTakeWithPropagator(String location) {
         Take take = new Take();
         take.setWeight(BigDecimal.TEN);
         take.setAmount(10);
         take.setLocation(location);
         take.setDate(ZonedDateTime.now());
-        take.setPropagator(createValidPropagator());
+        take.setBy(createValidPropagator());
         return take;
     }
 
@@ -139,14 +127,20 @@ public class CreateTakeStepDefs {
         take.setDate(ZonedDateTime.now());
         return take;
     }
+    /*
+    private Take createValidTakeWithDonor(String location) {
+        Take take = new Take();
+        take.setWeight(BigDecimal.TEN);
+        take.setAmount(10);
+        take.setLocation(location);
+        take.setDate(ZonedDateTime.now());
+
+        return take;
+    }
+    */
 
     private Propagator createValidPropagator() {
-        Propagator propagator = new Propagator();
-        propagator.setUsername("propagator");
-        propagator.setEmail("propagator@sample.app");
-        propagator.setPassword("password");
-        propagator.encodePassword();
-        return propagator;
+       return propagatorRepository.findById("propagator").get();
     }
 
     private List<String> getLocations() {
