0268fa459342e77df6b08e965d7481b56c4f6073
Ilyass Mahdiyan <ilyassmah00@gmail.com>
2023-03-24 16:59:32 +0100

    Funcionalities of DeleteDonationStepDefs implemented

--- src/test/java/cat/udl/eps/softarch/demo/steps/DeleteDonationStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/demo/steps/DeleteDonationStepDefs.java
@@ -1,8 +1,21 @@
 package cat.udl.eps.softarch.demo.steps;
 
+import cat.udl.eps.softarch.demo.domain.Donation;
+import cat.udl.eps.softarch.demo.domain.Request;
 import cat.udl.eps.softarch.demo.repository.DonationRepository;
 import cat.udl.eps.softarch.demo.repository.DonorRepository;
+import cat.udl.eps.softarch.demo.repository.PropagatorRepository;
+import cat.udl.eps.softarch.demo.repository.RequestRepository;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.When;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 public class DeleteDonationStepDefs {
     public static String newResourceUri;
@@ -12,4 +25,33 @@ public class DeleteDonationStepDefs {
     private DonorRepository donorRepository;
     @Autowired
     private DonationRepository donationRepository;
+
+    @When("^I delete the donation")
+    public void deleteRequest() throws Exception {
+        stepDefs.result = stepDefs.mockMvc.perform(
+                        delete(newResourceUri)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print());
+    }
+
+    @And("There is a donation created with amount {int}, weight {int} and location {string}")
+    public void thereIsADonationCreatedWithAmountWeightAndLocation(int amount, int weight, String location) {
+        Donation donation = new Donation();
+        donation.setAmount(amount);
+        donation.setWeight(new BigDecimal(weight));
+        donation.setLocation(location);
+        donation.setBy(donorRepository.findById("userdonor").get());
+        donation = donationRepository.save(donation);
+        newResourceUri = "/donations/" + donation.getId();
+    }
+
+    @And("I try to get the donation")
+    public void iTryToGetTheDonation() throws Exception {
+        stepDefs.result = stepDefs.mockMvc.perform(
+                        get(newResourceUri)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print());
+    }
 }
