7686edda496fccdf983714028be9f515356626a9
Ilyass Mahdiyan <ilyassmah00@gmail.com>
2023-04-13 16:55:02 +0200

    Add Test Donation Object to DB Initialization

--- src/main/java/cat/udl/eps/softarch/demo/config/DBInitialization.java
+++ src/main/java/cat/udl/eps/softarch/demo/config/DBInitialization.java
@@ -1,13 +1,17 @@
 package cat.udl.eps.softarch.demo.config;
+import cat.udl.eps.softarch.demo.domain.Donation;
 import cat.udl.eps.softarch.demo.domain.Donor;
 import cat.udl.eps.softarch.demo.domain.Propagator;
 import cat.udl.eps.softarch.demo.domain.User;
+import cat.udl.eps.softarch.demo.repository.DonationRepository;
 import cat.udl.eps.softarch.demo.repository.DonorRepository;
 import cat.udl.eps.softarch.demo.repository.PropagatorRepository;
 import cat.udl.eps.softarch.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import javax.annotation.PostConstruct;
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
 import java.util.Arrays;
 
 @Configuration
@@ -22,11 +26,15 @@ public class DBInitialization {
 
     private final PropagatorRepository propagatorRepository;
 
-    public DBInitialization(UserRepository userRepository, DonorRepository donorRepository, PropagatorRepository propagatorRepository) {
+    private final DonationRepository donationRepository;
+
+    public DBInitialization(UserRepository userRepository, DonorRepository donorRepository, PropagatorRepository propagatorRepository,
+                            DonationRepository donationRepository) {
 
         this.userRepository = userRepository;
         this.donorRepository = donorRepository;
         this.propagatorRepository = propagatorRepository;
+        this.donationRepository = donationRepository;
     }
 
     @PostConstruct
@@ -61,6 +69,15 @@ public class DBInitialization {
             donor.encodePassword();
             donorRepository.save(donor);
         }
+
+        //Default donation
+        Donation donation = new Donation();
+        donation.setAmount(89);
+        donation.setWeight(new BigDecimal("11.34"));
+        donation.setDate(ZonedDateTime.now());
+        donation.setLocation("Lleida");
+        donation.setBy(donorRepository.findById("userdonor").get());
+        donationRepository.save(donation);
 
         if (Arrays.asList(activeProfiles.split(",")).contains("test")) {
             // Testing instances
